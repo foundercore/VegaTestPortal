677a0e41442977f88a901f2360baff1e24b0209c
snipextt <snipextt@gmail.com>
2021-04-08 22:58:24 +0530

    Fixed router transitions

--- src/app/core/app-animation.module.ts
+++ src/app/core/app-animation.module.ts
@@ -1,25 +1,31 @@
 import {
-    trigger,
-    transition,
-    style,
-    query,
-    animate,
+  trigger,
+  transition,
+  style,
+  query,
+  animate,
 } from '@angular/animations';
-  
-export const fade =
-  trigger('routeAnimations', [
-    transition('* <=> *', [
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          left: 'auto',
-          width: '100%',
-          opacity: 0,
-          transform: 'scale(0) translateY(100%)',
-        }),
-      ]),
-      query(':enter', [
-        animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
-      ])
+
+export const fade = trigger('routeAnimations', [
+  transition('* <=> *', [
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        left: 'auto',
+        width: '96%',
+        opacity: 0,
+        transform: 'scale(0) translateY(100%)',
+      }),
     ]),
+    query(
+      ':enter',
+      [
+        animate(
+          '600ms ease',
+          style({ opacity: 1, transform: 'scale(1) translateY(0)' })
+        ),
+      ],
+      { optional: true }
+    ),
+  ]),
 ]);
