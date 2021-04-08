02434dab0d307a782389e606d1389359bd251068
snipextt <snipextt@gmail.com>
2021-04-08 22:28:09 +0530

    Added basic authentication

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,47 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/authentication/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   username = '';
   password = '';
   pageTitle = 'LOGIN';
   userNamePlaceHolder = 'ramesh@education.com';
   maskPassword = true;
   constructor(
+    private authService: AuthService,
     private router: Router,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login(): void {
     if (this.username === 'admin' && this.password === 'admin') {
       this.router.navigate(['user']);
     } else {
       //alert('Invalid credentials');
-      this.toastr.success("Invalid credentials");
+      this.toastr.success('Invalid credentials');
     }
   }
 
-  isFormValid(){
+  isFormValid() {
     return this.username.length > 0 && this.password.length > 0;
   }
 
   onSubmit(form: NgForm) {
     console.log('User Name is : ' + form.value.username);
-    if (form.value.username === 'admin' && form.value.password === 'admin') {
-      this.router.navigate(['home']);
-    } else {
-      // alert('Invalid credentials');
-       this.toastr.error("Invalid credentials");
-    }
+    localStorage.setItem(
+      'authInfo',
+      btoa(`${form.value.username}:${form.value.password}`)
+    );
+    this.authService
+      .loggedIn()
+      .then(() => this.router.navigate(['home']))
+      .catch(() => this.toastr.error('Invalid credentials'));
   }
 }
-
