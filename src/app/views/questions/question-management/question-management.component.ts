3bef546fa31f8f3dfe237a01d94822543b7e8865
Mayur <nsolanki@gmail.com>
2021-06-02 16:28:07 +0530

    Changes in Questions Management
    Questions Management
    o	By default Sort questions by LastUpdatedTime.
    o	After Upload the question list should refresh.
    o	Add a file name filter in the questions filter section. It should be a text field to be mapped with 'filename' field
    o	Remove the Description column

--- src/app/views/questions/question-management/question-management.component.ts
+++ src/app/views/questions/question-management/question-management.component.ts
@@ -33,7 +33,6 @@ export class QuestionManagementComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
     'select',
     'name',
-    'description',
     'subject',
     'explanation_added',
     'tags',
@@ -59,6 +58,7 @@ export class QuestionManagementComponent implements OnInit, AfterViewInit {
     tagCntrl: new FormControl(),
     topicCntrl: new FormControl(),
     substopicCntrl: new FormControl(),
+    fileNameCntrl: new FormControl(),
   });
 
   isFilterApply = false;
@@ -172,6 +172,7 @@ export class QuestionManagementComponent implements OnInit, AfterViewInit {
     this.filterGroup.controls['tagCntrl'].reset();
     this.filterGroup.controls['topicCntrl'].reset();
     this.filterGroup.controls['substopicCntrl'].reset();
+    this.filterGroup.controls['fileNameCntrl'].reset();
     this.isFilterApply = false;
     this.resetPaging();
     const searchQuestion =  this.createSearchObject();
@@ -211,10 +212,12 @@ export class QuestionManagementComponent implements OnInit, AfterViewInit {
       searchQuestion.subject = this.filterGroup.controls['subjectCntrl'].value;
       else if(this.filterGroup.controls['tagCntrl'].value !== null && this.filterGroup.controls['tagCntrl'].value.length !== 0)
       searchQuestion.tags = this.filterGroup.controls['tagCntrl'].value;
-      // else if(this.filterGroup.controls['topicCntrl'].value !== null && this.filterGroup.controls['topicCntrl'].value.length !== 0)
-      // searchQuestion.topic = this.filterGroup.controls['topicCntrl'].value;
-      // else if(this.filterGroup.controls['substopicCntrl'].value !== null && this.filterGroup.controls['substopicCntrl'].value.length !== 0)
-      // searchQuestion.nameRegexPattern = this.filterGroup.controls['substopicCntrl'].value;
+      else if(this.filterGroup.controls['topicCntrl'].value !== null && this.filterGroup.controls['topicCntrl'].value.length !== 0)
+      searchQuestion.topic = this.filterGroup.controls['topicCntrl'].value;
+      else if(this.filterGroup.controls['substopicCntrl'].value !== null && this.filterGroup.controls['substopicCntrl'].value.length !== 0)
+      searchQuestion.subTopic = this.filterGroup.controls['substopicCntrl'].value;
+      else if(this.filterGroup.controls['fileNameCntrl'].value !== null && this.filterGroup.controls['fileNameCntrl'].value.length !== 0)
+      searchQuestion.filename = this.filterGroup.controls['fileNameCntrl'].value;
     }
 
     return searchQuestion;
@@ -247,7 +250,7 @@ export class QuestionManagementComponent implements OnInit, AfterViewInit {
   openBulkUploadDialog() {
     const dialogRef = this.dialog.open(QuestionBulkUploadDialogComponent);
     dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
+      this.resetFilter();
     });
   }
 
