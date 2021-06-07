e166c7665b78b1029f096aa73272e95d3e6b17f1
snipextt <snipextt@gmail.com>
2021-06-07 15:56:47 +0530

    Remove extra brace

--- src/app/views/user/add-user-dialog/add-user-dialog.component.ts
+++ src/app/views/user/add-user-dialog/add-user-dialog.component.ts
@@ -12,8 +12,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthorizationService } from 'src/app/services/authorization/authorization.service';
 import { PasswordPolicy } from '../change-password/password-policy';
 
-
-
 @Component({
   selector: 'app-add-user-dialog',
   templateUrl: './add-user-dialog.component.html',
@@ -22,31 +20,35 @@ import { PasswordPolicy } from '../change-password/password-policy';
 export class AddUserDialogComponent implements OnInit {
   roleList: string[] = [];
 
-  userFormGroup = new FormGroup({
-    firstname: new FormControl('',[Validators.required]),
-    lastname: new FormControl('',[Validators.required]),
-    gender: new FormControl('',[Validators.required]),
-    roles: new FormControl([],[Validators.required]),
-    address: new FormControl('',[Validators.required]),
-    state: new FormControl('',[Validators.required]),
-    email: new FormControl('',[Validators.required,Validators.email]  ),
-    password: new FormControl('',[
-      // 1. Password Field is Required
-      Validators.required,
-      // 2. check whether the entered password has a number
-      PasswordPolicy.patternValidator(/\d/, { hasNumber: true }),
-      // 3. check whether the entered password has upper case letter
-      PasswordPolicy.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
-      // 4. check whether the entered password has a lower-case letter
-      PasswordPolicy.patternValidator(/[a-z]/, { hasSmallCase: true }),
-      // 5. check whether the entered password has a special character
-      PasswordPolicy.patternValidator(/[*@!#%&()^~{}]+/, { hasSpecialCharacters: true }),
-      // 6. Has a minimum length of 8 characters
-      Validators.minLength(8)
-
-    ]),
-    confirmPassword: new FormControl('',[Validators.required]),
-  },MustMatch)
+  userFormGroup = new FormGroup(
+    {
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      roles: new FormControl([], [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        // 1. Password Field is Required
+        Validators.required,
+        // 2. check whether the entered password has a number
+        PasswordPolicy.patternValidator(/\d/, { hasNumber: true }),
+        // 3. check whether the entered password has upper case letter
+        PasswordPolicy.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+        // 4. check whether the entered password has a lower-case letter
+        PasswordPolicy.patternValidator(/[a-z]/, { hasSmallCase: true }),
+        // 5. check whether the entered password has a special character
+        PasswordPolicy.patternValidator(/[*@!#%&()^~{}]+/, {
+          hasSpecialCharacters: true,
+        }),
+        // 6. Has a minimum length of 8 characters
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    MustMatch
+  );
 
   constructor(
     private userService: UserService,
@@ -60,27 +62,26 @@ export class AddUserDialogComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      console.log(this.data)
-      if(this.data){
-        this.userFormGroup.controls.firstname.setValue(this.data.firstName);
-        this.userFormGroup.controls.address.setValue(this.data.address);
-        this.userFormGroup.controls.gender.setValue(this.data.gender);
-        this.userFormGroup.controls.lastname.setValue(this.data.lastName);
-        this.userFormGroup.controls.roles.setValue(this.data.roles);
-        this.userFormGroup.controls.state.setValue(this.data.state);
-        this.userFormGroup.controls.email.setValue(this.data.email);
-        this.userFormGroup.controls.password.setValidators([]);
-        this.userFormGroup.controls.confirmPassword.setValidators([]);
+    console.log(this.data);
+    if (this.data) {
+      this.userFormGroup.controls.firstname.setValue(this.data.firstName);
+      this.userFormGroup.controls.address.setValue(this.data.address);
+      this.userFormGroup.controls.gender.setValue(this.data.gender);
+      this.userFormGroup.controls.lastname.setValue(this.data.lastName);
+      this.userFormGroup.controls.roles.setValue(this.data.roles);
+      this.userFormGroup.controls.state.setValue(this.data.state);
+      this.userFormGroup.controls.email.setValue(this.data.email);
+      this.userFormGroup.controls.password.setValidators([]);
+      this.userFormGroup.controls.confirmPassword.setValidators([]);
+      this.userFormGroup.controls.email.disable();
+      if (this.data.isView || !this.authorizationService.isAdmin) {
+        this.userFormGroup.controls.firstname.disable();
+        this.userFormGroup.controls.address.disable();
+        this.userFormGroup.controls.gender.disable();
+        this.userFormGroup.controls.lastname.disable();
+        this.userFormGroup.controls.roles.disable();
+        this.userFormGroup.controls.state.disable();
         this.userFormGroup.controls.email.disable();
-        if(this.data.isView || !this.authorizationService.isAdmin){
-          this.userFormGroup.controls.firstname.disable();
-          this.userFormGroup.controls.address.disable();
-          this.userFormGroup.controls.gender.disable();
-          this.userFormGroup.controls.lastname.disable();
-          this.userFormGroup.controls.roles.disable();
-          this.userFormGroup.controls.state.disable();
-          this.userFormGroup.controls.email.disable();
-        }
       }
     }
   }
