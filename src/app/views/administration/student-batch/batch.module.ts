655ce424b3f2ec7bd3b18575369fa22cb1ed8521
snipextt <snipextt@gmail.com>
2021-07-07 11:12:30 +0530

    Remove admin area for staff role

--- src/app/views/administration/student-batch/batch.module.ts
+++ src/app/views/administration/student-batch/batch.module.ts
@@ -4,21 +4,11 @@ import { AddBatchStudentComponent } from './add-batch-student/add-batch-student.
 import { AddBatchComponent } from './add-batch/add-batch.component';
 import { BulkUploadBatchStudentsComponent } from './bulk-upload-batch-students/bulk-upload-batch-students.component';
 import { StudentBatchManagementComponent } from './student-batch-management/student-batch-management.component';
-import { RouterModule, Routes } from '@angular/router';
-import { Role } from 'src/app/core/constants';
-import { RoleGuard } from 'src/app/guard/role.guard';
-import { UserManagementComponent } from '../user/user-management/user-management.component';
+import { RouterModule } from '@angular/router';
 import { VegaMaterialModule } from 'src/app/core/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: StudentBatchManagementComponent,
-    canActivate: [RoleGuard],
-    data: { roles: [Role.ADMIN, Role.STAFF] },
-  },
-];
+
 
 @NgModule({
   declarations: [
@@ -29,7 +19,6 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
     VegaMaterialModule,
     SharedModule
   ],
--- src/app/views/administration/user/user.module.ts
+++ src/app/views/administration/user/user.module.ts
@@ -3,21 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AddUserDialogComponent } from './add-user-dialog/add-user-dialog.component';
 import { UserBulkUploadDialogComponent } from './user-bulk-upload-dialog/user-bulk-upload-dialog.component';
 import { UserManagementComponent } from './user-management/user-management.component';
-import { Routes, RouterModule } from '@angular/router';
-import { Role } from 'src/app/core/constants';
-import { RoleGuard } from 'src/app/guard/role.guard';
-import { StudentBatchManagementComponent } from '../student-batch/student-batch-management/student-batch-management.component';
+import { RouterModule } from '@angular/router';
 import { VegaMaterialModule } from 'src/app/core/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: UserManagementComponent,
-    canActivate: [RoleGuard],
-    data: { roles: [Role.ADMIN, Role.STAFF] },
-  },
-];
 
 @NgModule({
   declarations: [
@@ -28,7 +16,6 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes),
     VegaMaterialModule,
     SharedModule
   ],
