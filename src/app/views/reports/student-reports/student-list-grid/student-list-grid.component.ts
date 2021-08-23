8c2fd14324fb7002b9725ec3153f7cd6fde9aa2f
Sharad1994 <sharadkhandelwal2209@gmail.com>
2021-08-23 19:45:25 +0530

    Bug Fixes
    Bug Fixes :
    Adding search bar on Test Name on student report screen.
    resizing of students solutions screen.

--- src/app/views/reports/student-reports/student-list-grid/student-list-grid.component.ts
+++ src/app/views/reports/student-reports/student-list-grid/student-list-grid.component.ts
@@ -1,5 +1,11 @@
 import { BreadcrumbNavService } from './../../../layout/breadcrumb/breadcrumb-nav.service';
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -15,19 +21,24 @@ import { ToastrService } from 'ngx-toastr';
   selector: 'app-student-list-grid',
   templateUrl: './student-list-grid.component.html',
   styleUrls: ['./student-list-grid.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StudentListGridComponent implements OnInit {
-
   showGrid = true;
   currentSectionSubmittedData: any;
   studentName: string = '';
   studentId: string = '';
   resultData;
+  input: string;
 
   public pageOptions = PAGE_OPTIONS;
 
-  displayedColumns: string[] = ['testName', 'attempted', 'marksObtained','actions'];
+  displayedColumns: string[] = [
+    'testName',
+    'attempted',
+    'marksObtained',
+    'actions',
+  ];
 
   dataSource = new MatTableDataSource<any>();
 
@@ -42,73 +53,89 @@ export class StudentListGridComponent implements OnInit {
     private toastr: ToastrService,
     private ref: ChangeDetectorRef
   ) {
-      router.events.subscribe((val) => {
-        // see also
-        if(val instanceof NavigationEnd){
-          if(val.url.includes('assignment_report')){
-            this.showGrid = false;
-          }else{
-            this.showGrid = true;
-          }
-          ref.detectChanges();
+    router.events.subscribe((val) => {
+      // see also
+      if (val instanceof NavigationEnd) {
+        if (val.url.includes('assignment_report')) {
+          this.showGrid = false;
+        } else {
+          this.showGrid = true;
         }
+        ref.detectChanges();
+      }
     });
   }
 
   ngOnInit(): void {
-
     this.activatedRoute.params.subscribe((params) => {
-        this.getAssignments(params.student_id);
-        this.studentName = params.student_name;
-        this.studentId = params.student_id;
-    })
-  }
-
-  getAssignments(username) {
-    this.testAssignmentService.getAssignmentByUsername(username).subscribe((resp) => {
-      this.resultData = resp;
-      this.dataSource = new MatTableDataSource<any>(this.resultData);
-      this.dataSource.paginator = this.paginator;
+      this.getAssignments(params.student_id);
+      this.studentName = params.student_name;
+      this.studentId = params.student_id;
+      this.clear();
     });
   }
 
+  getAssignments(username) {
+    this.testAssignmentService
+      .getAssignmentByUsername(username)
+      .subscribe((resp) => {
+        this.resultData = resp;
+        this.dataSource = new MatTableDataSource<any>(this.resultData);
+        this.dataSource.paginator = this.paginator;
+      });
+  }
 
+  viewResult(row: any) {
+    console.log(
+      '/home/reports/student/selected-student/' +
+        this.studentId +
+        '/' +
+        this.studentName +
+        '/assignment_report/' +
+        row.assignmentId
+    );
+    this.router
+      .navigate([
+        '/home/reports/student/selected-student/' +
+          this.studentId +
+          '/' +
+          this.studentName +
+          '/assignment_report/' +
+          row.assignmentId,
+      ])
+      .then(() => console.log('Navigate to score card'))
+      .catch((err) => console.log('Error=> Navigate to score card=>', err));
+  }
+
+  resetTest(row: any) {
+    console.log(row);
+    const dialogRef = this.dialog.open(DialogConformationComponent, {
+      disableClose: true,
+      data: { primaryBtnText: 'Close', secondaryBtnText: 'Reset' },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == 'delete') {
+        this.testAssignmentService
+          .resetStudentAssignment(row.assignmentId, this.studentId)
+          .subscribe(
+            (resp) => {
+              this.toastr.success(`Successfully test is reset.`);
+              this.getAssignments(this.studentId);
+            },
+            (error) => {
+              this.toastr.error('Unable to reset test');
+            }
+          );
+      }
+    });
+  }
+
+  clear() {
+    this.input = '';
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-
-
-  viewResult(row: any) {
-    console.log('/home/reports/student/selected-student/' + this.studentId+"/" + this.studentName + '/assignment_report/' + row.assignmentId);
-    this.router
-    .navigate(['/home/reports/student/selected-student/' + this.studentId+"/" + this.studentName + '/assignment_report/' + row.assignmentId])
-    .then(() => console.log('Navigate to score card'))
-    .catch((err) =>
-      console.log('Error=> Navigate to score card=>', err)
-    );
-  }
-
-  resetTest(row: any){
-    console.log(row)
-    const dialogRef = this.dialog.open(DialogConformationComponent, { disableClose: true,data: {primaryBtnText:'Close',secondaryBtnText:'Reset'}});
-    dialogRef.afterClosed().subscribe((result) => {
-      if(result == 'delete'){
-
-        this.testAssignmentService.resetStudentAssignment(row.assignmentId,this.studentId).subscribe(resp => {
-          this.toastr.success(`Successfully test is reset.`);
-          this.getAssignments(this.studentId);
-        },error =>{
-          this.toastr.error('Unable to reset test');
-        })
-      }
-    });
   }
 }
-
